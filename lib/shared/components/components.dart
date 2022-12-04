9173c8afb14b0a1b152b1bc99588231950ea7042
Mariam Moamen <mar@xtnd.net>
2022-12-04 19:02:20 +0200

    web view and finished app

--- lib/shared/components/components.dart
+++ lib/shared/components/components.dart
@@ -1,55 +1,61 @@
 import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:news_app_flutter/modules/web_view/web_view.dart';
 
-Widget buildArticleItem(articles, context) => Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Row(
-        children: [
-          Container(
-            width: 120.0,
-            height: 120.0,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10.0),
-              image: DecorationImage(
-                image: NetworkImage(
-                  articles['urlToImage'] ?? "",
-                ),
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          const SizedBox(
-            width: 10.0,
-          ),
-          Expanded(
-            child: Container(
+Widget buildArticleItem(articles, context) => InkWell(
+      onTap: () {
+        navigateTo(context, WebViewScreen(url: articles['url']));
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Row(
+          children: [
+            Container(
+              width: 120.0,
               height: 120.0,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Expanded(
-                    child: Text(
-                      articles['title'],
-                      maxLines: 3,
-                      overflow: TextOverflow.ellipsis,
-                      style: Theme.of(context).textTheme.bodyText1,
-                    ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                image: DecorationImage(
+                  image: NetworkImage(
+                    articles['urlToImage'] ?? "",
                   ),
-                  Text(
-                    articles['publishedAt'],
-                    style: const TextStyle(color: Colors.grey),
-                  ),
-                ],
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          ),
-        ],
+            const SizedBox(
+              width: 10.0,
+            ),
+            Expanded(
+              child: Container(
+                height: 120.0,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Text(
+                        articles['title'],
+                        maxLines: 3,
+                        overflow: TextOverflow.ellipsis,
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
+                    ),
+                    Text(
+                      articles['publishedAt'],
+                      style: const TextStyle(color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
 
-Widget articleBuilder(list) => ConditionalBuilder(
+Widget articleBuilder(list,{isSearch = false}) => ConditionalBuilder(
       //ليه بعمل الكوندشن كده ومبعملش الكوندكش على اللودينج علشان لما اللودينج بيدي ايرور بس انه مبيحلقش يجيب الداتا علشان تتعرض
       condition: list.isNotEmpty,
       builder: (BuildContext context) => ListView.separated(
@@ -61,8 +67,7 @@ Widget articleBuilder(list) => ConditionalBuilder(
                 width: double.infinity,
                 color: Colors.grey,
               )),
-      fallback: (BuildContext context) =>
-          const Center(child: CircularProgressIndicator()),
+      fallback: (BuildContext context) => isSearch? Container(): const Center(child: CircularProgressIndicator()),
     );
 
 void navigateTo(context, widget) {
